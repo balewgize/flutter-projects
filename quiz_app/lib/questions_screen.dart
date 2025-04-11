2704003525fa187a27bb5cbc7b2de6e8ab9560c6
Alemnew Marie <alemnewmarie461@gmail.com>
2025-04-11 12:02:23 +0300

    add questions model and data

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -12,6 +12,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(child: Text('Questions Screen'));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("This is quesion..."),
+          SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 1")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 2")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 3")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 4")),
+        ],
+      ),
+    );
   }
 }
