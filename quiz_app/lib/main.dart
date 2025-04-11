3cffd044e139d53fca55410fc9aa95e4d724d138
Alemnew Marie <alemnewmarie461@gmail.com>
2025-04-11 10:32:26 +0300

    add quiz and qustionds screens

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.indigo],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+      print("======================================");
+      print(activeScreen);
+      print("======================================");
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.indigo],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
