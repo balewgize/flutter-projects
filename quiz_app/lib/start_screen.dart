d750db3f4f094ec2183158f01d0e7315352f1f45
Alemnew Marie <alemnewmarie461@gmail.com>
2025-04-11 09:45:06 +0300

    Add btn icon and image opacity

--- quiz_app/lib/start_screen.dart
+++ quiz_app/lib/start_screen.dart
@@ -9,17 +9,29 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
           SizedBox(height: 80),
           Text(
             "Learn Flutter the fun the way!",
-            style: TextStyle(color: Colors.white, fontSize: 24),
+            style: TextStyle(
+              color: const Color.fromARGB(255, 225, 220, 220),
+              fontSize: 24,
+            ),
           ),
           SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text("Start Quiz"),
+            icon: const Icon(
+              Icons.arrow_right_alt,
+              size: 26,
+              color: Color.fromARGB(255, 231, 226, 226),
+            ),
+            label: const Text("Start Quiz", style: TextStyle(fontSize: 18)),
           ),
         ],
       ),
